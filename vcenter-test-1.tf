692e255acd79cf3851ebb40404c206393bab6e04
bdwheeler <bdwheeler@gmail.com>
2018-03-06 15:15:53 -0700

    removing a vm for testing

--- vcenter-test-1.tf
+++ vcenter-test-1.tf
@@ -60,31 +60,31 @@ resource "vsphere_virtual_machine" "terraform-test-1" {
   }
 }
 
-resource "vsphere_virtual_machine" "terraform-test-2" {
-  name = "terraform-test-2"
-  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
-  datastore_id = "${data.vsphere_datastore.datastore.id}"
-  num_cpus = 2
-  memory = 1024
-  guest_id = "ubuntu64Guest"
-
-  network_interface {
-    network_id = "${data.vsphere_network.network.id}"
-  }
-
-  disk {
-    label = "disk0"
-    thin_provisioned = true
-    size  = 20
-  }
-
-  clone {
-    template_uuid = "${data.vsphere_virtual_machine.template.id}"
-  }
-
-  connection {
-    type = "ssh"
-    user = "cloud-user"
-    password = "m0rp#3us!"
-  }
-}
+#resource "vsphere_virtual_machine" "terraform-test-2" {
+#  name = "terraform-test-2"
+#  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+#  datastore_id = "${data.vsphere_datastore.datastore.id}"
+#  num_cpus = 2
+#  memory = 1024
+#  guest_id = "ubuntu64Guest"
+#
+#  network_interface {
+#    network_id = "${data.vsphere_network.network.id}"
+#  }
+#
+#  disk {
+#    label = "disk0"
+#    thin_provisioned = true
+#    size  = 20
+#  }
+#
+#  clone {
+#    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+#  }
+#
+#  connection {
+#    type = "ssh"
+#    user = "cloud-user"
+#    password = "m0rp#3us!"
+#  }
+#}
