2c1bdef2bf041004e753a5d94fb3d388d1d0b537
bdwheeler <bdwheeler@gmail.com>
2020-11-11 09:37:43 -0700

    fixing variable inputs

--- gm-samples/modules/SecretsManager/provider.tf
+++ gm-samples/modules/SecretsManager/provider.tf
@@ -1,20 +1,6 @@
-/*
-provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws-us-gov:iam::676984028973:role/SOC_EC2FullAccess_Role"
-  }
-  region    = "us-gov-west-1"
-}
 
 provider "aws" {
-  region     = "us-gov-west-1"
-  access_key = ""   
-  secret_key = ""
-}
-*/
-
-provider "aws" {
-  region     = "${var.soc_region}"   
-  access_key = "${var.soc_access_key}"   
-  secret_key = "${var.soc_secret_key}"
+  region     = var.soc_region
+  access_key = var.soc_access_key
+  secret_key = var.soc_secret_key
 }
