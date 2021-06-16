20e62b775d4a67502bc6abb6c5bfef663a94d217
bdwheeler <bdwheeler@gmail.com>
2021-06-16 13:38:01 -0600

    update to efs-test

--- bunge-samples/efs-test/main.tf
+++ bunge-samples/efs-test/main.tf
@@ -1,4 +1,4 @@
-# variables
+#variables
 variable "access_key" {
   sensitive = true
 }
@@ -17,14 +17,14 @@ variable "environment" {
   default = "<%=input.instanceContext%>"
 }
 
-# providers
+#providers
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
   region = "us-east-1"
 }
 
-# resources
+#resources
 resource "aws_efs_file_system" "efs" {
   performance_mode = "generalPurpose"
   throughput_mode = "bursting"
