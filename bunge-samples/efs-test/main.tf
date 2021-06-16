7aeffe509f0119b3033e770459312776c4108162
bdwheeler <bdwheeler@gmail.com>
2021-06-16 13:25:00 -0600

    adding efs stuff

---
+++ bunge-samples/efs-test/main.tf
@@ -0,0 +1,35 @@
+#variables
+variable "access_key" {
+  sensitive = true
+}
+
+variable "secret_key" {
+  sensitive = true
+}
+
+variable "efsName" {
+  type = string
+  default = "<%=customOptions.shareName%>"
+}
+
+variable "environment" {
+  type = string
+  default = "<%=input.instanceContext%>"
+}
+
+### Initialize Providers
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "us-east-1"
+}
+
+### resource
+resource "aws_efs_file_system" "efs" {
+  performance_mode = "generalPurpose"
+  throughput_mode = "bursting"
+  tags = {
+    Name = var.efsName
+    Environment = var.environment
+  }
+}
