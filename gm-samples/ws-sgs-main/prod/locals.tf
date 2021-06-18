1dfd7758a814a453630335d353456d8ab69283ae
bdwheeler <bdwheeler@gmail.com>
2021-06-18 11:27:25 -0600

    adding sg examples

---
+++ gm-samples/ws-sgs-main/prod/locals.tf
@@ -0,0 +1,9 @@
+locals {
+  ws_prod_web_sg_id = "sg-028e4ca18de8e2966"
+  ws_prod_app_sg_id = "sg-0d61201e83734b274"
+  ws_prod_db_sg_id  = "sg-0ac9f13fc446b3cf9"
+
+  
+  rfs_prod_app_cidrs = ["10.56.26.0/26", "10.56.26.64/26", "10.56.26.128/26"]
+}
+
